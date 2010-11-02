a6ccd146fa2a47bcbcbb24f3b162a7d834e08e06
Kent Gustavsson <kent.gustavsson@johnbauer.nu>
2010-11-02 21:24:39 +0100

    Oups forgotten the challange files

---
+++ RoboTek/Challange.complex.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace RoboTek
+{
+    partial class Challange
+    {
+        Map _map;
+        Form1 _form1;
+        public List<int> line_numbers = new List<int>();
+        public Challange(Map m, Form1 f1)
+        {
+            _map = m;
+            _form1 = f1;
+
+        }
+        public void Counter()
+        {
+            StackTrace st = new StackTrace(true);
+
+            int x = st.GetFrames().Count() - 1;
+            int n = 1;
+            string function_name = "";
+            function_name = st.GetFrame(n).GetMethod().Name;
+            int linenumber = st.GetFrame(n).GetFileLineNumber();
+            while (function_name != "Run")
+            {
+                if (!line_numbers.Contains(linenumber))
+                {
+                    line_numbers.Add(linenumber);
+                    //MessageBox.Show("Found " + function_name + " called at: " + linenumber);
+                }
+                n++;
+                function_name = st.GetFrame(n).GetMethod().Name;
+                linenumber = st.GetFrame(n).GetFileLineNumber();
+
+            }
+            
+
+        }
+        public void _wait()
+        {
+            while (!_map.getPlayer().isIdle())
+            {
+                System.Threading.Thread.Sleep(80);
+            }
+        }
+
+        public void forward()
+        {
+            _wait();
+            Counter();
+            _map.getPlayer().moveForward();
+        }
+
+    }
+}
---
+++ RoboTek/Challange.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RoboTek
+{
+    partial class Challange
+    {
+        void f1()
+        {
+
+            forward();
+            forward();
+
+        }
+
+        void f2()
+        {
+            f1();
+        }
+
+
+        public void Run()
+        {
+            f2();
+
+            Console.WriteLine("wtf");
+        }
+    }
+}
